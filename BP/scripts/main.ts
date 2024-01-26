e233a4651087437ae0a93ddff1fe1ebb6a22f046
ExDrill <72329798+ExDrill@users.noreply.github.com>
2024-01-25 23:28:31 -0500

    changes

--- BP/scripts/main.ts
+++ BP/scripts/main.ts
@@ -1,2 +1,11 @@
 import { world } from '@minecraft/server'
 import Gamemode from './modes/gamemode'
+import Deathmatch from './modes/deathmatch'
+
+/**
+ * Registry for gamemodes 
+ */
+const gamemodes: Gamemode[] = [
+    new Deathmatch()
+]
+
