6fa8b2125f5602e34080da8f1ad519508672c7f8
ExDrill <72329798+ExDrill@users.noreply.github.com>
2024-02-05 13:46:48 -0500

    More work on Archer

--- BP/scripts/main.ts
+++ BP/scripts/main.ts
@@ -2,10 +2,15 @@ import { world } from '@minecraft/server'
 import Deathmatch from './modes/deathmatch'
 import TeamDeathmatch from './modes/teamDeathmatch'
 import * as Lobby from './mechanics/lobby'
+
+import PlayerClass from './playerClasses/playerClass'
+import ArcherClass from './playerClasses/archer'
+
 import Command from './commands/command'
 import EndCommand from './commands/end'
 import StartCommand from './commands/start'
 import SetCountdownCommand from './commands/set_countdown'
+import EquipCommand from './commands/equip'
 
 /**
  * Registry for gamemodes 
@@ -24,8 +29,13 @@ export const propertyGamemodes = {
 export const commands = {
     end: new EndCommand(),
     start: new StartCommand(),
-    countdown: new SetCountdownCommand()
+    countdown: new SetCountdownCommand(),
+    equip: new EquipCommand()
 }
+
+export const playerClasses: PlayerClass[] = [
+    new ArcherClass()
+]
 
 world.afterEvents.worldInitialize.subscribe(Lobby.initEndRound)
 world.afterEvents.playerJoin.subscribe(Lobby.removeVoteOnJoin)
