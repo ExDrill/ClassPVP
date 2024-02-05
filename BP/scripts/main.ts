29782b1d9224b1e3ea64bfa0ae8b03391a931f43
QuackImpala7321 <74572567+QuackImpala7321@users.noreply.github.com>
2024-02-05 16:17:04 -0600

    cleaned up unused code from merge conflict

--- BP/scripts/main.ts
+++ BP/scripts/main.ts
@@ -7,10 +7,6 @@ import PlayerClass from './playerClasses/playerClass'
 import ArcherClass from './playerClasses/archer'
 
 import Command from './commands/command'
-import EndCommand from './commands/end'
-import StartCommand from './commands/start'
-import SetCountdownCommand from './commands/set_countdown'
-import EquipCommand from './commands/equip'
 
 /**
  * Registry for gamemodes 
@@ -18,19 +14,6 @@ import EquipCommand from './commands/equip'
 export const gamemodes = {
     'Deathmatch': new Deathmatch(),
     'Team Deathmatch': new TeamDeathmatch()
-}
-
-export const propertyGamemodes = {
-    'none': 'none',
-    'Deathmatch': 'deathmatch',
-    'Team Deathmatch': 'team_deathmatch'
-}
-
-export const commands = {
-    end: new EndCommand(),
-    start: new StartCommand(),
-    countdown: new SetCountdownCommand(),
-    equip: new EquipCommand()
 }
 
 export const playerClasses: PlayerClass[] = [
