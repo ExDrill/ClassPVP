060ffa94c0dc9c219401ece5f711287d490bff69
QuackImpala7321 <74572567+QuackImpala7321@users.noreply.github.com>
2024-02-06 18:21:50 -0600

    round timer entity ticks the world so it doesn't despawn

--- BP/scripts/modes/gamemode.ts
+++ BP/scripts/modes/gamemode.ts
@@ -25,7 +25,6 @@ export default abstract class Gamemode {
         this.assignTeams()
         Gamemode.setRoundTime(this.roundDurationTicks)
         Bossbar.createBossbarEntity()
-
     }
 
     public endRound(): void {
